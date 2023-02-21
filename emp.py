4f130573f243b78ec5262aed586664db71b9e292
Andreas Patakis <andreas@pop-os.localdomain>
2023-02-21 10:27:24 +0200

    implented command line arguments functionality

--- emp.py
+++ emp.py
@@ -3,6 +3,7 @@ from pprint import pprint
 import paramiko
 from scp import SCPClient
 from commands import Interface
+from utilities import parse_file, parse_args
 from _version import __version__
 
 art = """
@@ -17,15 +18,45 @@ from prompt_toolkit import PromptSession
 from prompt_toolkit.history import FileHistory
 from prompt_toolkit.auto_suggest import AutoSuggestFromHistory
 import readline
+import argparse
 import traceback
 from termcolor import colored
 import shlex
+import sys
 
 print(art)
 
 #initializing CommandsClient object
 interface = Interface()
 input_session = PromptSession(history=FileHistory('.inp_history'))
+
+#available actions 
+actions = {
+    '-f': parse_file,
+    '-i': parse_args
+    }
+
+#executing commands if given as command line arguments
+#sys.argv will be greater than 2 only if command line arguments are given
+if len(sys.argv) > 2:
+    PROGRAM,ACTION,TARGET = sys.argv
+    try:
+        if ACTION in actions:
+            actions_dict = actions[ACTION](TARGET)
+            for action in actions_dict:
+                try:
+                    func = getattr(interface, 'command_'+action['command'])
+                    print('\nExecuting {} {}'.format(action['command'], action['args']))
+                    func(*shlex.shlex(action['args']))
+                except Exception as e: print(e)
+        else:
+            print(f'Argument "{ACTION}" does not exist')
+    except ValueError:
+        print('Wrong format')
+    except Exception as e:
+        print(e)
+    sys.exit()
+
 
 
 while 1:
---
+++ utilities.py
@@ -0,0 +1,33 @@
+from pathlib import Path
+
+def parse_args(target:list) ->  list:
+    """
+    Parses the arguments and returns a dict of commands and args
+    """
+    """TODO: Can an argument have only one command?"""
+    commands = []
+    processes = target.split(';')
+    for i, process in enumerate(processes):
+        command_args = process.strip().split(' ',1)
+        command = command_args.pop(0)
+        args = '' if not command_args else command_args[0].strip()
+        commands.append(
+            {
+                'order':i+1,
+                'command':command,
+                'args':args
+            })
+    return commands
+
+def parse_file(path:str) -> list:
+    """
+    Parses commands from file
+    """
+    print(path)
+    with open(Path(path)) as f:
+        lines = [line.rstrip() for line in f]
+    commands = ';'.join(lines)
+    return parse_args(commands)
+
+
+    

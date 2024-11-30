5ed484d623f2fcfc08a8aee4b90fa98caae590da
giorgostheo <giorgostheo97@gmail.com>
2024-11-30 19:04:15 +0200

    versioning validation and extra modules

---
+++ modules/hello_world/main.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import numpy as np
+
+print('Everyhting works')
+print("Pandas version ->",pd.__version__)
+print("Numpy version ->", np.__version__)
+print(pd.read_csv("data/data.csv"))
